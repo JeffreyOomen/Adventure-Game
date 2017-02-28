31893a66801345060344f61df6bf7c60ac3efdce
brambooo <bram_reinold@hotmail.com>
2017-02-28 15:35:40 +0100

    push -u origin master

---
+++ src/main/java/nl/avans/ivh11/a2b/ProftaakApplication.java
@@ -0,0 +1,12 @@
+package nl.avans.ivh11.a2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProftaakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProftaakApplication.class, args);
+    }
+}
---
+++ src/test/java/nl/avans/ivh11/a2b/ProftaakApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.avans.ivh11.a2b;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProftaakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
