444db0f1ac0e5181e366004aa594c14c53006e88
Matthijs Overboom <matthijsske@gmail.com>
2017-03-07 20:56:06 +0100

    Added opponent specific logger to see if this might be a good solution

--- src/main/java/nl/avans/ivh11/a2b/domain/util/observer/TextBasedOpponentLogger.java
+++ src/main/java/nl/avans/ivh11/a2b/domain/util/observer/TextBasedOpponentLogger.java
@@ -6,12 +6,12 @@ import nl.avans.ivh11.a2b.domain.util.Opponent;
 /**
  * Created by matthijs on 6-3-17.
  */
-public class TextBasesOpponentLogger extends OpponentObserver {
+public class TextBasedOpponentLogger implements Observer {
     @Override
     public void update(Observable observable) {
         if(observable instanceof Character) {
-            Character subject = (Character) observable;
-            System.out.println(subject.);
+            Opponent subject = (Opponent) observable;
+            System.out.println(subject);
         }
     }
 }
