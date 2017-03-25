a21a5505319b4932744b69872790b7bff0bfdfba
Matthijs Overboom <matthijsske@gmail.com>
2017-03-25 17:51:00 +0100

    Added configuration for transactional

--- src/main/java/nl/avans/ivh11/a2b/presentation/controller/BattleController.java
+++ src/main/java/nl/avans/ivh11/a2b/presentation/controller/BattleController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class BattleController
 {
     @Autowired
--- src/main/java/nl/avans/ivh11/a2b/presentation/controller/CharacterController.java
+++ src/main/java/nl/avans/ivh11/a2b/presentation/controller/CharacterController.java
@@ -6,12 +6,14 @@ import nl.avans.ivh11.a2b.service.OpponentService;
 import nl.avans.ivh11.a2b.service.SecurityService;
 import nl.avans.ivh11.a2b.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@PreAuthorize("isAuthenticated()")
 public class CharacterController
 {
     @Autowired
