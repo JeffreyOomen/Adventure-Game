584f8d48a9285683d6fd189d0007c609d3f628f1
Jeffrey Oomen <jeffrey_oomen@outlook.com>
2017-04-01 18:30:03 +0200

    removed EquipmentEnum class

--- src/main/java/nl/avans/ivh11/a2b/domain/util/EquipmentEnum.java
+++
@@ -1,9 +0,0 @@
-package nl.avans.ivh11.a2b.domain.util;
-
-/**
- * Represents the different EquipmentRepository Pieces
- */
-public enum EquipmentEnum
-{
-    HELMET, TORSO, LEGS, BOOTS, GLOVES, WEAPON, SWORD, STAFF, BOW
-}
