1720a4b1a31bab963d14a3eb1e7796eaa8953303
Inbar Azulay <inbar07897@gmail.com>
2017-04-03 14:35:28 +0200

    BattleServiceTest working

--- src/main/java/nl/avans/ivh11/a2b/service/BattleServiceImplMock.java
+++ src/main/java/nl/avans/ivh11/a2b/service/BattleServiceImplMock.java
@@ -1,4 +1,5 @@
 package nl.avans.ivh11.a2b.service;
+
 import lombok.Getter;
 import lombok.Setter;
 import nl.avans.ivh11.a2b.datastorage.character.CharacterRepository;
@@ -10,7 +11,6 @@ import nl.avans.ivh11.a2b.domain.enemy.Enemy;
 import nl.avans.ivh11.a2b.domain.usable.Usable;
 import nl.avans.ivh11.a2b.domain.util.CustomRandom;
 import nl.avans.ivh11.a2b.domain.util.Opponent;
-import nl.avans.ivh11.a2b.domain.util.Stats;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,34 +19,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import javax.annotation.PostConstruct;
+
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-@Service("battleService")
+
 @Repository
 @Getter
 @Setter
-public class BattleServiceImpl implements BattleService
+public class BattleServiceImplMock implements BattleService
 {
     private static final String BREAK = "<br/><br/>";
+
     private CharacterRepository characterRepository;
     private EnemyRepository enemyRepository;
     private UsableRepository usableRepository;
+
     private final CharacterService characterService;
     private final EnemyService enemyService;
-    private final EntityManagerFactory entityManagerFactory;
+
     private Battle battle;
+
     @Autowired
-    public BattleServiceImpl(CharacterRepository characterRepo, EnemyRepository enemyRepo, UsableRepository usableRepo, CharacterService characterService, EnemyService enemyService, EntityManagerFactory entityManagerFactory) {
+    public BattleServiceImplMock(CharacterRepository characterRepo, EnemyRepository enemyRepo, UsableRepository usableRepo, CharacterService characterService, EnemyService enemyService) {
         this.characterRepository = characterRepo;
         this.enemyRepository = enemyRepo;
         this.usableRepository = usableRepo;
+
         this.characterService = characterService;
         this.enemyService = enemyService;
-        this.entityManagerFactory = entityManagerFactory;
+
+
     }
+
     /**
      * Setup a new Battle between two Opponent's. Setting up includes
      * making a Battle Object. This Battle Object will observe the actions
@@ -58,10 +63,13 @@ public class BattleServiceImpl implements BattleService
     public Opponent setupBattle(Opponent character) {
         // generate a random enemy
         Opponent enemy = this.randomEnemy(character);
+
         // setup battle between character and enemy
         this.battle = new Battle(character, enemy);
+
         return enemy;
     }
+
     /**
      * Teardown the Battle between the two Opponent's. Tearing down includes
      * giving the Character XP, saving the state of both Character and Enemy
@@ -69,9 +77,10 @@ public class BattleServiceImpl implements BattleService
      */
     private void teardownBattle(Character character) {
         character.setState(character.getNormalState());
-        this.saveBattleState(); // save current state
+//        this.saveBattleState(); // save current state
         this.battle = null; // clear memory allocation
     }
+
     /**
      * Report about the events happened in the battle during the player's actions.
      * @return A String containing battle report information
@@ -79,54 +88,32 @@ public class BattleServiceImpl implements BattleService
     @Transactional
     public String battleReport() {
         String battleReport = "";
+
         //First add attack messages before adding possible victory messages
         for (String message: this.battle.getMessages()) {
             battleReport += message + BREAK;
         }
         this.battle.getMessages().clear();
+
         if (!this.battle.getEnemy().isAlive()) {
             // give out XP to the character
             this.battle.getCharacter().receiveXp(this.getBattle().getEnemy().getHitpoints());
+
             for (String message: this.battle.getMessages()) {
                 battleReport += message + BREAK;
             }
-            battleReport += this.handleEnemyDrop();
-            this.teardownBattle((Character) this.battle.getCharacter());
         }
+
+        this.teardownBattle((Character) this.battle.getCharacter());
+
         // clear messages to prevent duplicates
         if(this.battle != null) {
             this.battle.getMessages().clear();
         }
+
         return battleReport;
     }
-    /**
-     * Programmatically (without using declarative approach @Transactional) handle
-     * an Enemy drop. This includes randomly creating a drop, assigning it to a
-     * Character, and logging to the battle report.
-     * @return a modified battle report
-     * @throws HibernateException when something goes wrong
-     */
-    private String handleEnemyDrop() throws HibernateException {
-        String battleReport = "";
-        Session session = this.entityManagerFactory.unwrap(SessionFactory.class).openSession();
-        try {
-            session.beginTransaction();
-            Enemy enemy = (Enemy)this.battle.getEnemy();
-            Character character = (Character) this.battle.getCharacter();
-            Usable randomDrop = enemy.randomDrop();
-            session.save(randomDrop);
-            characterService.addInventoryItem(character, randomDrop);
-            battleReport += "<span class=\"message-warning\">" + enemy.getName() + " just dropped " + randomDrop.getName() + BREAK;
-            // commit the transaction
-            session.getTransaction().commit();
-        } catch (HibernateException e1) {
-            rollback(session);
-            throw e1;
-        } finally {
-            close(session);
-        }
-        return battleReport;
-    }
+
     /**
      * Executes a battle event with a normal attack for the Character
      */
@@ -135,6 +122,7 @@ public class BattleServiceImpl implements BattleService
         this.battle.getCharacter().setActionBehavior(new NormalAttack());
         this.doAction();
     }
+
     /**
      * Executes a battle event with a special attack for the Character
      */
@@ -143,6 +131,7 @@ public class BattleServiceImpl implements BattleService
         this.battle.getCharacter().setActionBehavior(new SpecialAttack());
         this.doAction();
     }
+
     /**
      * Executes a battle event with a heal for the Character
      */
@@ -151,45 +140,29 @@ public class BattleServiceImpl implements BattleService
         this.battle.getCharacter().setActionBehavior(new Heal());
         this.doAction();
     }
+
     /**
      * Execute a battle action
      */
     private void doAction() {
         // let the character attack
         this.battle.playTurn(new ActionCommand(this.battle.getCharacter(), this.battle.getEnemy()));
-        // let the enemy attack
-        this.battle.playTurn(new ActionCommand(this.battle.getEnemy(), this.battle.getCharacter()));
+
+        if (this.battle.getEnemy().isAlive()) {
+            // let the enemy attack
+            this.battle.playTurn(new ActionCommand(this.battle.getEnemy(), this.battle.getCharacter()));
+        }
+
         Character character = (Character) this.battle.getCharacter();
         Random r = new Random();
-        if (r.nextInt(20) == 1 && this.battle.getCharacter().isAlive() && this.battle.getEnemy().isAlive()) {
+        if (r.nextInt(20) == 1 && this.battle.getEnemy().isAlive() && this.battle.getCharacter().isAlive()) {
             List<String> messages = this.battle.getMessages();
             character.setState(character.getWeakenedState());
             messages.add(this.battle.getEnemy().getName() + " has just set you back to your weakened state");
             this.battle.setMessages(messages);
         }
-        // save the battle state
-        this.saveBattleState();
     }
-    /**
-     * Saves the state of the Character and the Enemy caused by the Battle.
-     */
-    private void saveBattleState() {
-        Session session = this.entityManagerFactory.unwrap(SessionFactory.class).openSession();
-        try {
-            session.beginTransaction();
-            Enemy enemy = (Enemy)this.battle.getEnemy();
-            Character character = (Character) this.battle.getCharacter();
-            session.saveOrUpdate(enemy);
-            session.saveOrUpdate(character);
-            // commit the transaction
-            session.getTransaction().commit();
-        } catch (HibernateException e1) {
-            rollback(session);
-            throw e1;
-        } finally {
-            close(session);
-        }
-    }
+
     /**
      * Generates a random Opponent based on the Stats of the other Opponent.
      * @param character an Opponent
@@ -200,29 +173,5 @@ public class BattleServiceImpl implements BattleService
         Opponent enemy = enemies.get(CustomRandom.getInstance().randomEnemy(enemies.size()));
         enemy.setStats(CustomRandom.getInstance().randomEnemyStats(character));
         return enemy;
-    }
-    /**
-     * Closes the Hibernate Session Connection
-     * @param session a session in which a transaction is started
-     */
-    private static void close(Session session) {
-        try {
-            session.close();
-        } catch (HibernateException e) {
-            System.err.println("Could not close the session: " + e);
-        }
-    }
-    /**
-     * Rollback the Transaction which happened inside the Session.
-     * This must be done when a Transaction has failed.
-     * @param session a session in which a transaction is started
-     */
-    private static void rollback(Session session) {
-        try {
-            Transaction tx = session.getTransaction();
-            tx.rollback();
-        } catch (HibernateException e) {
-            System.err.println("Could not rollback the session: " + e);
-        }
     }
 }
---
+++ src/test/java/nl/avans/ivh11/a2b/BattleServiceMockTests.java
@@ -0,0 +1,93 @@
+package nl.avans.ivh11.a2b;
+
+import nl.avans.ivh11.a2b.datastorage.character.CharacterRepository;
+import nl.avans.ivh11.a2b.datastorage.enemy.EnemyRepository;
+import nl.avans.ivh11.a2b.datastorage.usable.UsableRepository;
+import nl.avans.ivh11.a2b.domain.battle.AttackUtil;
+import nl.avans.ivh11.a2b.domain.battle.NormalAttack;
+import nl.avans.ivh11.a2b.domain.character.Dwarf;
+import nl.avans.ivh11.a2b.domain.character.decoration.Mage;
+import nl.avans.ivh11.a2b.domain.character.state.NormalState;
+import nl.avans.ivh11.a2b.domain.enemy.Enemy;
+import nl.avans.ivh11.a2b.domain.usable.UsableType;
+import nl.avans.ivh11.a2b.domain.util.CustomRandom;
+import nl.avans.ivh11.a2b.domain.util.Media;
+import nl.avans.ivh11.a2b.domain.util.Opponent;
+import nl.avans.ivh11.a2b.domain.util.Stats;
+import nl.avans.ivh11.a2b.service.*;
+import nl.avans.ivh11.a2b.domain.character.Character;
+import org.hibernate.jpa.internal.EntityManagerFactoryImpl;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManagerFactory;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+
+public class BattleServiceMockTests {
+    private BattleService battleService;
+    private EnemyService enemyService;
+    private CharacterService characterService;
+
+    private CharacterRepository characterRepository;
+    private EnemyRepository enemyRepository;
+    private UsableRepository usableRepository;
+
+    private CustomRandom customRandom;
+
+    private Character character;
+    private Enemy enemy;
+
+    @Before
+    public void setup() {
+//        Mocking repositories and service
+        characterRepository = mock(CharacterRepository.class);
+        enemyRepository = mock(EnemyRepository.class);
+        usableRepository = mock(UsableRepository.class);
+        enemyService= mock(EnemyServiceImpl.class);
+        characterService = mock(CharacterServiceImpl.class);
+        customRandom = mock(CustomRandom.class);
+//        Creating battleService
+        battleService = new BattleServiceImplMock(characterRepository, enemyRepository, usableRepository, characterService, enemyService);
+        //        List of enemies for stub
+        List<Enemy> listOfEnemies = new ArrayList<>();
+        listOfEnemies.add(new Enemy(new Stats()));
+        listOfEnemies.add(new Enemy(new Stats()));
+        listOfEnemies.add(new Enemy(new Stats()));
+//        Stub for method calling in repository
+        when(enemyService.findAll()).thenReturn(listOfEnemies);
+//        Make new Character
+        character = new Dwarf("Jeffrey", new Stats(), new Media());
+        character.setAttackStyle(UsableType.EQUIPMENT_WEAPON_SWORD);
+        character.setState(NormalState.getInstance());
+//        Get the returned enemy from the battle that was set up
+        enemy = (Enemy) battleService.setupBattle(character);
+    }
+
+    @Test
+    public void AttackTest() {
+        enemy.setActionBehavior(new NormalAttack());
+        while(character.isAlive() && enemy.isAlive()) {
+            battleService.attack();
+        }
+        if (enemy.isAlive()) {
+            assertNotEquals(0, enemy.getCurrentHitpoints());
+            assertEquals(0, character.getCurrentHitpoints());
+        } else {
+            assertTrue(character.getHitpoints() >= character.getCurrentHitpoints());
+        }
+    }
+
+    @After
+    public void battleReportTest() {
+        battleService.battleReport();
+    }
+}
